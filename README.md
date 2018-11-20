e23ba94ed648893b0ddeea3f04b947c6c6eba69d
yooma <zhuleineuq@gmail.com>
2018-11-20 16:19:18 +0800

    java11 字符串增强

---
+++ README.md
@@ -0,0 +1,18 @@
+#### java7
+https://blog.csdn.net/chenleixing/article/details/47802653
+
+#### java8
+https://blog.csdn.net/liubenlong007/article/details/62039628
+
+#### java9
+http://developer.51cto.com/art/201801/564443.htm
+
+#### java10
+https://baijiahao.baidu.com/s?id=1594437679552808575&wfr=spider&for=pc
+
+#### java11
+https://blog.csdn.net/weixin_38055381/article/details/82865385
+
+#### 关于 HttpClient 的官方文档
+http://openjdk.java.net/groups/net/httpclient/recipes.html
+
--- src/com/yooma/java/java11/Main.java
+++ src/com/yooma/java/java11/Main.java
@@ -5,12 +5,13 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        feature01();
+//        feature01();
+
+        feature02();
     }
 
     /**
@@ -25,5 +26,26 @@ public class Main {
                 .build();
         var response = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
         System.out.println(response.body());
+    }
+
+    /**
+     * 字符串增强
+     */
+    public static void feature02() {
+        var str = "   hello   ";
+        // 长度为 0 或者只包含空格
+        System.out.println(str.isBlank());
+        // 长度为 0
+        System.out.println(str.isEmpty());
+        // 去除首尾空格
+        System.out.println(str.strip());
+        // 去掉尾部空格
+        System.out.println(str.stripTrailing());
+        // 去掉首部空格
+        System.out.println(str.stripLeading());
+        // 重复
+        System.out.println(str.repeat(3));
+        // 字符串转化为流，统计行数
+        System.out.println("a\nb\nc".lines().count());
     }
 }
